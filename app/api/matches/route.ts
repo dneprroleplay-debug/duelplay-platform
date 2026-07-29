7050fc77e291b62115f92eb56d148524796e13d4
Александр Бардаков <dneprroleplay@gmail.com>
2026-07-29 22:07:15 +0300

    Fix Match relations

--- app/api/matches/route.ts
+++ app/api/matches/route.ts
@@ -4,7 +4,11 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const matches = await prisma.match.findMany({
     include: {
-      creator: true,
+      playerOne: true,
+      playerTwo: true,
+      winner: true,
+      loser: true,
+      game: true,
     },
     orderBy: {
       createdAt: "desc",
